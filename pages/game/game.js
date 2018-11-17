923f60b7256e1e85da3ac3c07c7ef783d4ff3b85
405214669 <405214669@qq.com>
2018-11-17 16:43:58 +0800

    xxx

--- pages/game/game.js
+++ pages/game/game.js
@@ -1,1 +1,41 @@
-Page({})
+Page({
+  data: {
+    fishArray: [{
+      positionClass: 'fish_wrap right_left_4',
+      fishClass: 'fish_1',
+      id: 'fish_1'
+    }, {
+      positionClass: 'fish_wrap left_right_3',
+      fishClass: 'fish_6',
+      id: 'fish_2'
+    },{
+      positionClass: 'fish_wrap right_left_2',
+      fishClass: 'fish_1',
+      id: 'fish_3'
+    }],
+    directionTypes: [
+      'left_right_1',
+      'left_right_2',
+      'left_right_3',
+      'right_left_1',
+      'right_left_2',
+      'right_left_3',
+      'right_left_4'
+    ],
+    fishTyoes: [
+      'fish_1',
+      'fish_2',
+      'fish_6',
+      'fish_7',
+      'fish_8',
+      'fish_9',
+      'fish_10'
+    ]
+  },
+  onLoad: function() {
+  },
+
+  tapFish: function(event) {
+    console.log(event.currentTarget.id);
+  }
+})
