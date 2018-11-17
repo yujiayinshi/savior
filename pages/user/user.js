e05287e34bd6ae4268135d7926918021f8bbfc4d
黄凯 <yujiayinshi@gmail.com>
2018-11-17 16:45:10 +0800

    用户中心积分兑换

--- pages/user/user.js
+++ pages/user/user.js
@@ -3,7 +3,23 @@ const app = getApp()
 Page({
   data: {
     userInfo: {},
-    points: 0
+    points: 0,
+    products: [{
+      id: 1,
+      name: '香港海洋公园门票',
+      img: 'https://cloud-minapp-2654.cloud.ifanrusercontent.com/hkhy.jpg',
+      price: 300
+    }, {
+      id: 2,
+      name: '珠海长隆海洋王国门票',
+      img: 'https://cloud-minapp-2654.cloud.ifanrusercontent.com/zhcl.jpg',
+      price: 300
+      }, {
+        id: 3,
+        name: '深圳小梅沙海洋馆门票',
+        img: 'https://cloud-minapp-2654.cloud.ifanrusercontent.com/szxms.jpg',
+        price: 170
+      }]
   },
   onLoad: function () {
     this.setData({
