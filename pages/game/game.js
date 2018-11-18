a047c60fbddbe841f25aba78577c356a21f00262
黄凯 <yujiayinshi@gmail.com>
2018-11-18 10:37:39 +0800

    游戏主界面

--- pages/game/game.js
+++ pages/game/game.js
@@ -1,17 +1,80 @@
+const uuid = require('../../utils/util.js').uuid
+const app = getApp()
 Page({
   data: {
+    showad: false,
     fishArray: [{
-      positionClass: 'fish_wrap right_left_4',
+      positionClass: 'fish_wrap left_right_2',
       fishClass: 'fish_1',
       id: 'fish_1'
     }, {
-      positionClass: 'fish_wrap left_right_3',
-      fishClass: 'fish_6',
-      id: 'fish_2'
-    },{
-      positionClass: 'fish_wrap right_left_2',
+      positionClass: 'fish_wrap left_right_2',
       fishClass: 'fish_1',
+      id: 'fish_1'
+    }, {
+      positionClass: 'fish_wrap left_right_2',
+      fishClass: 'fish_1',
+      id: 'fish_1'
+    }, {
+      positionClass: 'fish_wrap left_right_2',
+      fishClass: 'fish_1',
+      id: 'fish_1'
+    }, {
+      positionClass: 'fish_wrap right_left_4',
+      fishClass: 'fish_2',
+      id: 'fish_2'
+    }, {
+      positionClass: 'fish_wrap right_left_4',
+      fishClass: 'fish_2',
+      id: 'fish_2'
+    }, {
+      positionClass: 'fish_wrap right_left_4',
+      fishClass: 'fish_2',
+      id: 'fish_2'
+    }, {
+      positionClass: 'fish_wrap right_left_4',
+      fishClass: 'fish_2',
+      id: 'fish_2'
+    }, {
+      positionClass: 'fish_wrap right_left_2',
+      fishClass: 'fish_3',
       id: 'fish_3'
+    }, {
+      positionClass: 'fish_wrap right_left_2',
+      fishClass: 'fish_4',
+      id: 'fish_4'
+    }, {
+      positionClass: 'fish_wrap right_left_2',
+      fishClass: 'fish_5',
+      id: 'fish_5'
+    }, {
+      positionClass: 'fish_wrap right_left_3',
+      fishClass: 'fish_6',
+      id: 'fish_6'
+    }, {
+      positionClass: 'fish_wrap right_left_3',
+      fishClass: 'fish_6',
+      id: 'fish_6'
+    }, {
+      positionClass: 'fish_wrap left_right_1',
+      fishClass: 'fish_7',
+      id: 'fish_7'
+    }, {
+      positionClass: 'fish_wrap left_right_1',
+      fishClass: 'fish_7',
+      id: 'fish_7'
+    }, {
+      positionClass: 'fish_wrap left_right_3',
+      fishClass: 'fish_8',
+      id: 'fish_8'
+    }, {
+      positionClass: 'fish_wrap left_right_1',
+      fishClass: 'fish_9',
+      id: 'fish_9'
+    }, {
+      positionClass: 'fish_wrap left_right_2',
+      fishClass: 'fish_10',
+      id: 'fish_10'
     }],
     directionTypes: [
       'left_right_1',
@@ -30,12 +93,112 @@ Page({
       'fish_8',
       'fish_9',
       'fish_10'
+    ],
+    garbageImgs: [
+      'https://cloud-minapp-2654.cloud.ifanrusercontent.com/bottle.png',
+      'https://cloud-minapp-2654.cloud.ifanrusercontent.com/drink.png',
+      'https://cloud-minapp-2654.cloud.ifanrusercontent.com/bag.png',
+      'https://cloud-minapp-2654.cloud.ifanrusercontent.com/box.png',
+      'https://cloud-minapp-2654.cloud.ifanrusercontent.com/glass.png',
+      'https://cloud-minapp-2654.cloud.ifanrusercontent.com/dangerous.png'
+    ],
+    fishImgs: [
+      'https://cloud-minapp-2654.cloud.ifanrusercontent.com/fish1.gif',
+      'https://cloud-minapp-2654.cloud.ifanrusercontent.com/fish2.gif',
+      'https://cloud-minapp-2654.cloud.ifanrusercontent.com/fish3.gif',
+      'https://cloud-minapp-2654.cloud.ifanrusercontent.com/fish4.gif',
+      'https://cloud-minapp-2654.cloud.ifanrusercontent.com/fish5.gif'
     ]
   },
   onLoad: function() {
+    this.setData({
+      garbages: this.createGarbage(10),
+      fishes: this.createFish(10)
+    })
+    var me = this
+    setInterval(function() {
+      const garbages = me.data.garbages.map(item => {
+        return {
+          id: item.id,
+          img: item.img,
+          left: Math.round((Math.random() * 2)) + item.left - 1,
+          top: Math.round((Math.random() * 2)) + item.top - 1
+        }
+      })
+      me.setData({
+        garbages: garbages
+      })
+    }, 100)
+    setInterval(function() {
+      let num = 0
+      if (me.data.garbages.length < 10) {
+        num = 1
+      }
+      if (me.data.garbages.length < 6) {
+        num = 2
+      }
+      if (me.data.garbages.length < 2) {
+        num = 3
+      }
+      if (num > 0) {
+        me.setData({
+          garbages: me.data.garbages.concat(me.createGarbage(1))
+        })
+      }
+    }, 1000)
+    setInterval(function() {
+      me.data.garbages.forEach(function(item, index) {
+        if (item.left < 0 || item.left > wx.getSystemInfoSync().windowWidth
+          || item.top < 0 || item.top > wx.getSystemInfoSync().windowHeight) {
+           me.data.garbages.splice(index, 1)
+          } 
+      })
+      me.setData({garbages: me.data.garbages})
+    }, 3000)
   },
-
-  tapFish: function(event) {
-    console.log(event.currentTarget.id);
+  createFish: function(number) {
+    let result = []
+    for (var i = 0; i < number; i++) {
+      result.push({
+        id: uuid(),
+        img: this.data.fishImgs[Math.floor(Math.random() * this.data.fishImgs.length)],
+        left: Math.round(Math.random() * wx.getSystemInfoSync().windowWidth),
+        top: Math.round(Math.random() * wx.getSystemInfoSync().windowHeight)
+      })
+    }
+    return result
+  },
+  createGarbage: function(number) {
+    let result = []
+    for (var i = 0; i < number; i++) {
+      result.push({
+        id: uuid(),
+        img: this.data.garbageImgs[Math.round(Math.random() * this.data.garbageImgs.length)],
+        left: Math.round(Math.random() * wx.getSystemInfoSync().windowWidth),
+        top: Math.round(Math.random() * wx.getSystemInfoSync().windowHeight)
+      })
+    }
+    return result
+  },
+  tapgarbage: function(event) {
+    console.log('event.currentTarget', event.currentTarget)
+    const img = event.currentTarget.dataset.img
+    if (img.indexOf('dangerous') > -1) {
+      this.setData({
+        showad: true
+      })
+      var me = this
+      setTimeout(function() {
+        me.setData({
+          showad: false
+        })
+      }, 5000)
+    }
+    const id = event.currentTarget.id
+    this.data.garbages.splice(this.data.garbages.findIndex(item => item.id === id), 1)
+    app.globalData.points += 1;
+    this.setData({
+      points: app.globalData.points
+    })
   }
 })
--- pages/user/user.js
+++ pages/user/user.js
@@ -8,22 +8,43 @@ Page({
       id: 1,
       name: '香港海洋公园门票',
       img: 'https://cloud-minapp-2654.cloud.ifanrusercontent.com/hkhy.jpg',
-      price: 300
+      price: 3000
     }, {
       id: 2,
       name: '珠海长隆海洋王国门票',
       img: 'https://cloud-minapp-2654.cloud.ifanrusercontent.com/zhcl.jpg',
-      price: 300
+      price: 3000
       }, {
         id: 3,
         name: '深圳小梅沙海洋馆门票',
         img: 'https://cloud-minapp-2654.cloud.ifanrusercontent.com/szxms.jpg',
-        price: 170
+        price: 300
       }]
   },
   onLoad: function () {
     this.setData({
-      userInfo: app.globalData.userInfo
+      userInfo: app.globalData.userInfo,
+      points: app.globalData.points
     })
+  },
+  exchange: function (e) {
+    console.log(e.currentTarget)
+    const price = e.currentTarget.dataset.price
+    if (this.data.points >= price) {
+      this.setData({
+        points: this.data.points - price
+      })
+      wx.showModal({
+        title: '积分兑换成功',
+        content: '去看海洋动物吧',
+        showCancel: false
+      })
+    } else {
+      wx.showModal({
+        title: '积分不足',
+        content: '努力获取积分吧',
+        showCancel: false
+      })
+    }
   }
 })
